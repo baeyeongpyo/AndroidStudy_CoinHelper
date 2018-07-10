a1ac47d02eee1ddcbb46b6df458ccae7d8f74561
yeongpyo <byp93@naver.com>
2018-07-10 23:56:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yeongpyo/androidstudy_coinhelper/Coin1_Fragment.kt
@@ -0,0 +1,17 @@
+package com.example.yeongpyo.androidstudy_coinhelper
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Coin1_Fragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_coin1_, container, false)
+        return view
+    }
+
+
+}
