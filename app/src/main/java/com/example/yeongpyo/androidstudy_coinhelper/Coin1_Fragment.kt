8e26bb3ff084824afff3b00923e210219a430715
yeongpyo <byp93@naver.com>
2018-07-13 23:58:30 +0900

    ViewPager Add

--- app/src/main/java/com/example/yeongpyo/androidstudy_coinhelper/Coin1_Fragment.kt
+++ app/src/main/java/com/example/yeongpyo/androidstudy_coinhelper/Coin1_Fragment.kt
@@ -1,18 +1,51 @@
 package com.example.yeongpyo.androidstudy_coinhelper
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.yeongpyo.androidstudy_coinhelper.BaseUtil.BaseRecyclerAdapter
+import com.example.yeongpyo.androidstudy_coinhelper.BaseUtil.BaseRecyclerHolder
 
 class Coin1_Fragment : Fragment() {
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_coin1_, container, false)
+
+        (activity as? MainActivity)?.supportActionBar?.let {
+            it.title = " Coin list 1"
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+
+        val Coinlist1 = gettestAdpater1(view.findViewById(R.id.CoinList1))
+                .apply {
+                    data = arrayListOf(
+                            TestDB("Test1", "Test2"),
+                            TestDB("Test3", "Test4"),
+                            TestDB("Test5", "Test6"),
+                            TestDB("Test7", "Test8")
+                    )
+                }
+        view.findViewById<RecyclerView>(R.id.CoinList1).adapter = Coinlist1
+        view.findViewById<RecyclerView>(R.id.CoinList1).layoutManager = LinearLayoutManager(context)
 
         return view
     }
 
+    fun gettestAdpater1(listview: RecyclerView) = object : BaseRecyclerAdapter<TestDB>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+                object : BaseRecyclerHolder<TestDB>(R.layout.item_data, listview) {
+                    override fun onViewCreate(item: TestDB?): Unit = with(itemView) {
+                        findViewById<TextView>(R.id.Data1).text = item?.Data1
+                        findViewById<TextView>(R.id.Data2).text = item?.Data2
+                    }
+                }
+    }
 
 }
--- app/src/main/java/com/example/yeongpyo/androidstudy_coinhelper/MainActivity.kt
+++ app/src/main/java/com/example/yeongpyo/androidstudy_coinhelper/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.yeongpyo.androidstudy_coinhelper
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent.getActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.coinapi.APIinterface
-import com.example.coinapi.TradesData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.concurrent.thread
+import android.support.annotation.LayoutRes
+import android.support.v4.app.FragmentActivity
+import com.example.yeongpyo.androidstudy_coinhelper.Adapter.CustomPagerAdater
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setTitle("ssss")
 
+        fragmentlayout.adapter = CustomPagerAdater(supportFragmentManager)
+
+/*
         thread {
             val retrofit = Retrofit.Builder()
                     .baseUrl("https://api.coinone.co.kr")
@@ -46,7 +47,8 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
-
+    */
 
     }
+
 }
